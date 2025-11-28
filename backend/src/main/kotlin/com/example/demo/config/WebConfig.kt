aac5dae47f4b1bdd39fd6af457b3d125bcdb4fcf
jieun <jieun1111@yu.ac.kr>
2025-11-29 03:33:30 +0900

    Fix: Config files and package names

--- backend/src/main/kotlin/com/example/demo/config/WebConfig.kt
+++ backend/src/main/kotlin/com/example/demo/config/WebConfig.kt
@@ -1,4 +1,4 @@
-package com.example.demo.config
+package com.example.SEproject.project.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
